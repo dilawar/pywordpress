98d98171badb39cab1252e201abbf2ea2b2cfc3f
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-23 22:10:30 +0530

    Format.py is not needed

--- wordpress.py
+++ wordpress.py
@@ -22,10 +22,36 @@ import lxml.html as lh
 import codecs
 import errno
 import difflib 
-import format 
 
 # globals 
 blogDir = "."
+
+def formatWithNotChangeOnTag(txt, tag) :
+  newText = ""
+  beginPre = False 
+  endPre = True
+  preRegex = re.compile("\<"+tag+"\>")
+  endPreRegex = re.compile("\<\/"+tag+"pre\>")
+  for line in txt.split("\n") :
+    if len(line.strip()) == 0 : continue 
+    if preRegex.search(line) and endPreRegex.search(line) : continue
+    else : 
+      if preRegex.search(line) :
+        beginPre = True
+        endPre = False
+      if re.search(r"\<\/pre\>", line) :
+        endPre = True
+        beginPre = False
+    #check 
+    if beginPre is True and endPre is False:
+      newText += (line+"\n")
+    else : # format it 
+      newText += (line.strip()+" ")
+  return newText
+
+
+def formatContent(content) :
+  return formatWithNotChangeOnTag(content, "pre")
 
 def getTitle(txt) :
   titleRegex = re.compile("\<TITLE\>\s*(?P<title>[\w\s,]+)\s*\</TITLE\>",
@@ -67,7 +93,7 @@ def sendPostToWordpress(post, wp, txt) :
     m = contentRegex.search(txt)
     if m :
       content = m.groupdict()['content']
-      content = format.formatContent(content)
+      content = formatContent(content)
     else :
       print("[W] Post with empty content.")
       content = ""
