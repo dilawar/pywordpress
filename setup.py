8b17b901a49c475ba125e451072f0ff7df2a824c
Dilawar <dilawars@iitb.ac.in>
2013-12-23 17:15:45 +0000

    Begining to pack

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 def read(*paths):
- """Build a file path from *paths* and return the contents."""
+    """Build a file path from *paths* and return the contents."""
     with open(os.path.join(*paths), 'r') as f:
         return f.read()
 
